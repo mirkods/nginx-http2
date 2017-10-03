cef27c13b34011ab3f6a93af5b2a0c04c49746cf
Mirko <mirko.diserafino@gmail.com>
2017-10-03 09:28:38 +0200

    Removed pagespeed plugin

--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ NGINX_VERSION=1.12.1
 #openssl version
 OPENSSL_VERSION=1.0.2l
 
-#ngx_pagespeed version
-NPS_VERSION=1.9.32.3
-
 #Directories
 CURRENT_DIR=$(pwd)
 OPENSSL_DIR=$(pwd)/openssl-${OPENSSL_VERSION}
@@ -24,18 +21,6 @@ tar -xvf nginx-${NGINX_VERSION}.tar.gz
 curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 #Extracting openssl
 tar -xvf openssl-${OPENSSL_VERSION}.tar.gz
-#Save ngx_pagespeed module
-wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip
-#Extracting ngx_pagespeed module
-unzip release-${NPS_VERSION}-beta.zip
-#Entering in ngx_pagespeed
-cd ngx_pagespeed-release-${NPS_VERSION}-beta/
-#Downloading psol
-wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
-#Extracting psol
-tar -xzvf ${NPS_VERSION}.tar.gz 
-# Exiting from ngx_pagespeed
-cd ..
 
 #Entering inside the nginx folder
 cd nginx-${NGINX_VERSION}
@@ -45,8 +30,7 @@ cd nginx-${NGINX_VERSION}
 --with-http_ssl_module \
 --with-http_v2_module \
 --with-openssl=${OPENSSL_DIR} \
---with-cc-opt="-Wno-deprecated-declarations" \
---add-module=${CURRENT_DIR}/ngx_pagespeed-release-${NPS_VERSION}-beta
+--with-cc-opt="-Wno-deprecated-declarations"
 
 # Compiling everything on 64 bits systems
 KERNEL_BITS=64 make install
